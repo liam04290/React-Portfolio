1b224eba80d1f3ac5209cc0e46935bee5cead97e
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-01 01:51:03 -0400

    Added initial App.js code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        
+        <main>
+          <Switch>
+            {}
+            <Route exact path="/">
+              <Redirect to="/about" />
+            </Route>
+
+            {}
+            <Route path="/about" component={AboutMe} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/resume" component={Resume} />
+
+            {}
+            <Route path="*">
+              <Redirect to="/about" />
+            </Route>
+          </Switch>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
