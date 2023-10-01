cc7096afe89b3c09309bc6cd268a62c6e061271e
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-01 02:09:26 -0400

    Added code to src/index.js. Added placeholder code for Header.js, Footer.js, and AboutMe.js.

---
+++ portfolio/src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <header>
+      Placeholder About Me
+    </header>
+  );
+};
+
+export default AboutMe;
---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <header>
+      Placeholder Footer
+    </header>
+  );
+};
+
+export default Footer;
