3749f51491bdfbd81735af05764ddd7876013916
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-01 15:26:40 -0400

    Changed functionality of Footer.js.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/fontawesome-free-brands';
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="social-links">
-                <a href="linkedin.com/in/liamrobertson4u" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-                <a href="github.com/liam04290" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="social-links">
+
+        <a href="https://www.linkedin.com/in/liamrobertson4u" target="_blank" rel="noopener noreferrer">
+          <img src="li-icon.png" alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/liam04290" target="_blank" rel="noopener noreferrer">
+          <img src="gh-icon.png" alt="GitHub" />
+        </a>
+
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
