428992e6be5a95111badbba2ba17e5b1ab919415
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-01 04:20:20 -0400

    Added code for Header.js and Navigation.js. Simplified and corrected App.js based on acceptance criteria.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
@@ -11,29 +10,19 @@ import Resume from './components/Resume';
 
 function App() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/resume">Resume</Link>
-            {}
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutMe />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/resume" element={<Resume />} />
-            {}
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<AboutMe />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/resume" element={<Resume />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
     );
-  }
+}
 
 export default App;
